a00d976589cc5f70857ce8c96d052c93804ac411
Andrew Davis <andrew@andrewrdavis.net>
2012-03-12 21:26:59 -0400

    Initial commit with new permissions

---
+++ src/edu/kettering/autosilencer/AutoSilencerActivity.java
@@ -0,0 +1,13 @@
+package edu.kettering.autosilencer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AutoSilencerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
